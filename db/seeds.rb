b0b5e2176d90e9687acb504f3c63e66931f4c5d8
Yuexin Zhang <teresayuexinzhang@gmail.com>
2025-09-17 00:54:54 +0000

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Book.create(title: "The Great Gatsby", author: "F. Scott Fitzgerald", published_year: 1925)
-Book.create(title: "To Kill a Mockingbird", author: "Harper Lee", published_year: 1960)
-Book.create(title: "1984", author: "George Orwell", published_year: 1949)
-Book.create(title: "Pride and Prejudice", author: "Jane Austen", published_year: 1813)
-Book.create(title: "The Catcher in the Rye", author: "J.D. Salinger", published_year: 1951)
+Book.create(title: "The Great Gatsby", author: "F. Scott Fitzgerald", published_date: Date.new(1925, 1, 1))
+Book.create(title: "To Kill a Mockingbird", author: "Harper Lee", published_date: Date.new(1960, 1, 1))
+Book.create(title: "1984", author: "George Orwell", published_date: Date.new(1949, 1, 1))
+Book.create(title: "Pride and Prejudice", author: "Jane Austen", published_date: Date.new(1813, 1, 1))
+Book.create(title: "The Catcher in the Rye", author: "J.D. Salinger", published_date: Date.new(1951, 1, 1))
